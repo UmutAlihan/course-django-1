4c329bd2b9f9ae38279a9e0bf586166292c62028
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2021-11-27 22:05:16 +0300

    [section 3] more dynamic path converters (1-12 -> month conversion)

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,6 +1,21 @@
 from django.http import HttpResponse, HttpResponseNotFound
 
 
+monthly_challenges = {
+    "january": 'eat no meat for entire month',
+    "february": 'walk for at least 20 mins a day',
+    "march": 'march text is here',
+    "april": 'april challenge',
+    "may": 'may challenge',
+    "june": 'june challenge',
+    "july": 'july challenge',
+    "august": 'august challenge',
+    "september": 'september challenge',
+    "october": 'october challenge is here',
+    "november": 'november challenge',
+    "december": 'december challenge'
+}
+
 # Create your views here
 
 def monthly_challenge_by_number(request, month):
@@ -9,13 +24,8 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     ## month -> is the identifier as defined in brackets from challenges/urls.py
     ## multiple <..> dynamic url parameters -> can be extracted simply by name as parameters in view function (as kywargs)
-    challenge_text = None
-    if month == "january":
-        challenge_text = 'eat no meat for entire month'
-    elif month == "february":
-        challenge_text = 'walk for at least 20 mins a day'
-    elif month == "march":
-        challenge_text = 'march text is here'
-    else:
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
         return HttpResponseNotFound('<h1>This month is not supported</h1>')
-    return HttpResponse(challenge_text)
