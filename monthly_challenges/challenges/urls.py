2c264dd8d0cc4e6eabb8c3d5d62727720fa16cc3
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2021-11-27 15:07:17 +0300

    [Section 3] created first page for App challenges -> .../challenges/january

---
+++ monthly_challenges/challenges/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+# Create your views here.
+#from monthly_challenges.challenges import views
+from . import views
+
+urlpatterns = [
+    path('january', views.index, name='index'),
+]
