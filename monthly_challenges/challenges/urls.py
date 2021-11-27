bd51403bbe783dca0012aa6787dfb9727bd6859d
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2021-11-27 22:59:25 +0300

    [section 3] dynamic views, Urls, returning generated HTMLs without templates

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.index), # /challenges/
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge"),
     # <value> -> any text after /challenges/... is considered by views.mont..
