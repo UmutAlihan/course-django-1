d0a1a1a9aac52f83a1f09adc30c08f53c5c900ce
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2021-11-27 21:50:11 +0300

    add dynamic path segments and captured values

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -5,6 +5,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('january', views.january),
-    path('february', views.february),
+    path("<month>", views.monthly_challenge),  # <value> -> any text after /challenges/... is considered by views.mont..
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,10 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 
 # Create your views here
 
-def january(request):
-    return HttpResponse('eat no meat for entire month')
-
-def february(request):
-    return HttpResponse('walk for at least 20 mins a day')
+def monthly_challenge(request, month):
+    ## month -> is the identifier as defined in brackets from challenges/urls.py
+    ## multiple <..> dynamic url parameters -> can be extracted simply by name as parameters in view function (as kywargs)
+    challenge_text = None
+    if month == "january":
+        challenge_text = 'eat no meat for entire month'
+    elif month == "february":
+        challenge_text = 'walk for at least 20 mins a day'
+    elif month == "march":
+        challenge_text = 'march text is here'
+    else:
+        return HttpResponseNotFound('<h1>This month is not supported</h1>')
+    return HttpResponse(challenge_text)
