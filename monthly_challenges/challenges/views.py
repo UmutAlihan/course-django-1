3bf09cc853b9a9cf394dd588c2b02ce69f29b09a
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2021-11-28 11:47:00 +0300

    [section 4] if tag for conditional template content

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -15,7 +15,7 @@ monthly_challenges = {
     "september": 'september challenge',
     "october": 'october challenge is here',
     "november": 'november challenge',
-    "december": 'december challenge'
+    "december": None
 }
 
 # Create your views here
