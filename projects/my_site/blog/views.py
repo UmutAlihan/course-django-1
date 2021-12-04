eb9cade97990b3b24464ec33de32633218ffea87
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2021-12-04 17:09:29 +0300

    [section 5] add first template and create template dir

--- projects/my_site/blog/views.py
+++ projects/my_site/blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def starting_page(request):
-    pass #return render(request, 'blog/starting_page.html')
+    return render(request, 'blog/index.html') # this path is template for app
 
 
 def posts(request):
