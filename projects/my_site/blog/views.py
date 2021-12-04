ddbdceb9b67b521a65992b733db3ad56e6bffcad
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2021-12-04 16:25:49 +0300

    [section 5] create new blog page add URLs & views

---
+++ projects/my_site/blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def starting_page(request):
+    pass #return render(request, 'blog/starting_page.html')
+
+
+def posts(request):
+    pass
+
+def post_detail(request):
+    pass
