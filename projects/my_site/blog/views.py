4995769845064347bf939637a1327ed7526cc136
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2021-12-07 22:54:27 +0300

    [section 5] linking pages

--- projects/my_site/blog/views.py
+++ projects/my_site/blog/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render
 def starting_page(request):
     return render(request, 'blog/index.html') # this path is template for app
 
-
 def posts(request):
     return render(request, 'blog/all-posts.html')
 
