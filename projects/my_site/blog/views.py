c2f6782dc938c513b0ddb722c083268c0ff39ab4
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2021-12-07 23:09:12 +0300

    [section 5] adding dummy data to views file

--- projects/my_site/blog/views.py
+++ projects/my_site/blog/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from datetime import date
+
+
+posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpeg",
+        "author": "John Doe",
+        "date": date(2021, 7, 21),
+        "title": "Hike in the mountains",
+        "exerpt": """aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse""",
+        "content": """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
+                    qui officia deserunt mollit anim id est laborum."""
+    }
+]
 
 # Create your views here.
 
