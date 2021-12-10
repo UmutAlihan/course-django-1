67347ee155092c3d88dcdff3f90c803b3a2465cf
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2021-12-10 17:39:49 +0300

    [section 5] bugfix

--- projects/my_site/blog/views.py
+++ projects/my_site/blog/views.py
@@ -31,7 +31,31 @@ all_posts = [
         "slug": "hike-in-the-mountains_3",
         "image": "mountains.jpeg",
         "author": "John Doe",
+        "date": date(2018, 7, 20),
+        "title": "Hike in the mountains",
+        "exerpt": """aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse""",
+        "content": """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
+                qui officia deserunt mollit anim id est laborum."""
+    },
+{
+        "slug": "hike-in-the-mountains_3",
+        "image": "mountains.jpeg",
+        "author": "John Doe",
         "date": date(2020, 7, 20),
+        "title": "Hike in the mountains",
+        "exerpt": """aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse""",
+        "content": """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
+                qui officia deserunt mollit anim id est laborum."""
+    },
+{
+        "slug": "hike-in-the-mountains_3",
+        "image": "mountains.jpeg",
+        "author": "John Doe",
+        "date": date(2019, 7, 20),
         "title": "Hike in the mountains",
         "exerpt": """aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse""",
         "content": """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
