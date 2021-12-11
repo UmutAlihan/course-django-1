8ca1ae9f46af7a30c51bf9b05ca88976d439a362
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2021-12-11 20:10:57 +0300

    [section 6] getting all entries

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -6,6 +7,9 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=255) # field type
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     #created_at = models.DateTimeField(auto_now_add=True)
     #updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.rating}"
