991ac5f41f33896ec9986d5acf07fcce924f99c1
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2021-12-13 22:10:54 +0300

    [section 6] adding slugfield & overwriting save & slugify

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -12,11 +13,16 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False) # Harry Potter 1 => harry-potter-1
     #created_at = models.DateTimeField(auto_now_add=True)
     #updated_at = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.id])
+
+    def save(self, *args, **kwargs): # group all positiional and keyword arguments
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs) # making sure save method is still getting called
 
     def __str__(self):
         return f"{self.title} - {self.rating}"
