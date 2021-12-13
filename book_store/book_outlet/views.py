cc4be2891e4494bd0d443ed9eceb6a1f1f9fa49a
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2021-12-13 21:54:17 +0300

    [section 6] render details page

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from .models import Book
 
 # Create your views here.
 
 def index(request):
-    books = Book.objects.all()
+    try:
+        books = Book.objects.all()
+    except:
+        raise Http404()
     return render(request, 'book_outlet/index.html',
-                  context={"books": books
-                           })
+                  context={"books": books})
+
+def book_detail(request, id):
+    #try:
+    #    book = Book.objects.get(pk=id)
+    #except:
+    #    return Http404()
+    book = get_object_or_404(Book, pk=id)
+    return render(request, 'book_outlet/book_detail.html',
+                  context= {
+                      "title": book.title,
+                      "author": book.author,
+                      "rating": book.author,
+                      "is_bestseller": book.is_bestselling
+                  })
