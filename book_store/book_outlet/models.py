1dfc62d453ed741bf8bd143a3249da251a107719
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2021-12-13 22:02:14 +0300

    [section 6] Model URLs - overriding get_absolute_url & reverse()

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-
+from django.urls import reverse
 
 # Create your models here.
 
@@ -14,6 +14,9 @@ class Book(models.Model):
     is_bestselling = models.BooleanField(default=False)
     #created_at = models.DateTimeField(auto_now_add=True)
     #updated_at = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.id])
 
     def __str__(self):
         return f"{self.title} - {self.rating}"
