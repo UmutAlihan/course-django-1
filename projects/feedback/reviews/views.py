7a0d6e424ae422b7863ececfd28140f23e36a628
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2022-01-02 15:09:39 +0300

    [section 10] utilize submitted data, handling forms and exctracting data

--- projects/feedback/reviews/views.py
+++ projects/feedback/reviews/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def review(request):
+    if request.method == "POST":
+        entered_username = request.POST.get('username')
+        # post holds a dictionary where:
+            # keys are the names of the form fields
+            # values are the entered values
+        print(entered_username)
+        return HttpResponseRedirect("/thank-you")
+        # results new get request from client, which calls thank_you view and returns that rendered html page
+
     return render(request, "reviews/review.html")
+
+
+# mostly POST requests dont return rendered html page, instead redirects to another page
+def thank_you(request):
+    return render(request, 'reviews/thank_you.html')
