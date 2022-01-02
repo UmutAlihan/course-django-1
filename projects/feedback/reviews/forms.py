ca052681f667f9d1264295e2734029efd3976a60
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2022-01-02 16:06:37 +0300

    [section 10] customizing form controls and rendered htmls

--- projects/feedback/reviews/forms.py
+++ projects/feedback/reviews/forms.py
@@ -7,7 +7,8 @@ class ReviewForm(forms.Form):
 # no impact on the database
 # automatically generate html content when sent as response & rendered in browser
 # similarity to models definiton
-    user_name = forms.CharField(label="Your Name", max_length=100, error_messages= {
+    user_name = forms.CharField(label="Your Name", required=True, max_length=100, error_messages= {
         "required": "your name must not be empty!",
         "max_length": "please enter a shorter name!"
     })
+    #  detaylı bilgi almak için official django Docs'da Form fields reference'a gidebilirsin
