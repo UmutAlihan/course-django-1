9b8d72b341f3b6ed8f49b8a85315f58b7901d5eb
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2022-01-02 15:18:53 +0300

    [section 10] manuel form validations ands its problems

--- projects/feedback/reviews/views.py
+++ projects/feedback/reviews/views.py
@@ -9,11 +9,18 @@ def review(request):
         # post holds a dictionary where:
             # keys are the names of the form fields
             # values are the entered values
-        print(entered_username)
+        if entered_username == ""\
+                and len(entered_username) >= 100:
+            return render(request, "reviews/review.html", {
+                'has_error': True
+            })
         return HttpResponseRedirect("/thank-you")
         # results new get request from client, which calls thank_you view and returns that rendered html page
 
-    return render(request, "reviews/review.html")
+    return render(request, "reviews/review.html", {
+                'has_error': False
+            })
+
 
 
 # mostly POST requests dont return rendered html page, instead redirects to another page
