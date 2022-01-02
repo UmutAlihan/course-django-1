2005d1fcaba10f44517efdca6f445519601f232d
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2022-01-02 15:58:57 +0300

    [section 10] Validation with Django forms

--- projects/feedback/reviews/forms.py
+++ projects/feedback/reviews/forms.py
@@ -6,4 +6,8 @@ class ReviewForm(forms.Form):
 # justifying shape of the form with a different input fields in maight offer
 # no impact on the database
 # automatically generate html content when sent as response & rendered in browser
-    user_name = forms.CharField() # similarity to models
+# similarity to models definiton
+    user_name = forms.CharField(label="Your Name", max_length=100, error_messages= {
+        "required": "your name must not be empty!",
+        "max_length": "please enter a shorter name!"
+    })
--- projects/feedback/reviews/views.py
+++ projects/feedback/reviews/views.py
@@ -7,28 +7,18 @@ from .forms import ReviewForm
 
 def review(request):
     if request.method == "POST":
-        """entered_username = request.POST.get('username')
-        # post holds a dictionary where:
-            # keys are the names of the form fields
-            # values are the entered values"""
-        """if entered_username == ""\
-                and len(entered_username) >= 100:
-            return render(request, "reviews/review.html", {
-                'has_error': True
-            })"""
         form = ReviewForm(request.POST) # pass request to ReviewForm constructor
+
         if form.is_valid():
             print(form.cleaned_data)
             return HttpResponseRedirect("/thank-you")
             # results new get request from client, which calls thank_you view and returns that rendered html page
-
     else:
         form = ReviewForm()
 
     return render(request, "reviews/review.html", {
                 "form": form
             })
-
 
 
 # mostly POST requests dont return rendered html page, instead redirects to another page
