8f3c8046e7e6087507ac975062e5801f1fbb840f
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2022-01-09 14:27:49 +0300

    [section 12] making file upload work

--- projects/feedback/profiles/views.py
+++ projects/feedback/profiles/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+
+def store_file(file):
+
 
 
 class CreateProfileView(View):
@@ -9,4 +14,9 @@ class CreateProfileView(View):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        pass
+        # request.FILES -> gives access to uploaded files
+        # this comes from template form name="image"
+        # multiple assigned names can retrieve multiple files
+        print(request.FILES["image"])
+        return HttpResponseRedirect("/profiles")
+
