4150d10a5bbd92711933ee588c757fc0e36a78d5
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2022-01-09 14:58:31 +0300

    [section 12]adding a form with filefield

--- projects/feedback/profiles/views.py
+++ projects/feedback/profiles/views.py
@@ -2,21 +2,35 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 
+from .forms import ProfileForm
+
 # Create your views here.
 
 
 def store_file(file):
-
-
+    with open('temp/' + file.name, 'wb+') as destination:
+        for chunk in file.chunks(): # chunks() reads file one-by-one to avoid memory issues
+            destination.write(chunk)
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html",
+                      context= {"form": form}
+                      )
 
     def post(self, request):
-        # request.FILES -> gives access to uploaded files
-        # this comes from template form name="image"
-        # multiple assigned names can retrieve multiple files
-        print(request.FILES["image"])
-        return HttpResponseRedirect("/profiles")
+        submitted_form = ProfileForm(request.POST, request.FILES)  # create profile_form to pass data and files from request
+
+        if submitted_form.is_valid():
+            # request.FILES -> gives access to uploaded files
+            # this comes from template form name="image"
+            # multiple assigned names can retrieve multiple files
+            print(request.FILES["user_image"])
+            return HttpResponseRedirect("/profiles")
+
+        return render(request, "profiles/create_profile.html",
+                      context= {"form": submitted_form}
+                      )
+
 
