afeb023a6480dd4dc2663a9bf91e6456bedb7ad6
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2022-01-09 23:53:19 +0300

    [section 12] working with file field

--- projects/feedback/profiles/models.py
+++ projects/feedback/profiles/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class UserProfile(models.Model):
     # it only stores the path to the file in the database
-    user_image = models.ImageField(upload_to="images")
+    image = models.ImageField(upload_to="images")
--- projects/feedback/profiles/views.py
+++ projects/feedback/profiles/views.py
@@ -2,17 +2,20 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 
 from .forms import ProfileForm
 from .models import UserProfile
+
 
 # Create your views here.
 
 
 def store_file(file):
     with open('temp/' + file.name, 'wb+') as destination:
-        for chunk in file.chunks(): # chunks() reads file one-by-one to avoid memory issues
+        for chunk in file.chunks():  # chunks() reads file one-by-one to avoid memory issues
             destination.write(chunk)
+
 
 class CreateProfileView(CreateView):
     template_name = "profiles/create_profile.html"
@@ -46,3 +49,8 @@ class CreateProfileView(CreateView):
                       )
 """
 
+
+class ProfileView(ListView):
+    model = UserProfile
+    template_name = "profiles/user_profiles.html"
+    context_object_name = "profiles"
