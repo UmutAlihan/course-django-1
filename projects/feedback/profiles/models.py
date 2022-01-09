5807eb98ad9da8d202504dc21b9b37f45b8f486d
Alihan Dikel <alihan.dikel@turkcell.com.tr>
2022-01-09 15:13:54 +0300

    [section 12] using ImageField

--- projects/feedback/profiles/models.py
+++ projects/feedback/profiles/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class UserProfile(models.Model):
     # it only stores the path to the file in the database
-    image = models.FileField(upload_to="images")
+    image = models.ImageField(upload_to="images")
