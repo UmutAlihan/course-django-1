16b2cbb64a4c6bf8b30ebffb5a3cba62ab3e00e4
ALIHAN DIKEL <alihan.dikel@turkcell.com.tr>
2022-01-17 22:55:35 +0300

    [section 15] add AWS Elastic Beanstalk config file

--- projects/my_site/my_site/settings.py
+++ projects/my_site/my_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j@6(c#%wku#$oimn#))=pw!i_&c61p=(ka!*1*rp5sfj5luk%b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = getenv("IS_PRODUCTION", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv("APP_HOST")
+]
 
 
 # Application definition
