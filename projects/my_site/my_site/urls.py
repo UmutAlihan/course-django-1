68b80b50ac5b8da4189674312624dabe55b04950
ALIHAN DIKEL <alihan.dikel@turkcell.com.tr>
2022-01-17 22:44:30 +0300

    [section 15] set staticfiles with first easiest approach

--- projects/my_site/my_site/urls.py
+++ projects/my_site/my_site/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls")), # localhost:8000/blog/posts/my-first-post
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
